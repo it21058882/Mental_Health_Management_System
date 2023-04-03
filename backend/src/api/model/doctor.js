38e244fd185500a24d02f8385116d502836fbd7c
prawee11 <it21024740@my.sliit.lk>
2023-04-03 13:08:22 +0530

    Doctor Management System Backend

--- backend/src/api/model/doctor.js
+++ backend/src/api/model/doctor.js
@@ -2,10 +2,36 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const doctorSchema = new Schema({
-          name:{
-                    type : String,
-                    required : true
-          },
+    firstName: {
+        type: String,
+        required: true
+     },
+    lastName: {
+        type: String,
+        required: true
+     },
+    type :{
+         type : String,
+         required : true
+     },
+    gender:{
+        type : String,
+        required :true
+    },
+    email: {
+         type: String,
+         required: true
+    },
+    specialization :{
+        type: String,
+         required: true
+    },
+    image: {
+        type: String,
+        
+    }
+            
+                    
        
 })
 
