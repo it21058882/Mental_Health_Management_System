1f482b0d1b34c89a09651ac7e7b3b67a0d7e112a
Shanaka Minindu <it21058882@my.sliit.lk>
2023-05-16 22:53:58 +0530

    AdminHomePage edited

--- frontend/src/adminHomePage.js
+++ frontend/src/adminHomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function AdminHomePage(){
           return(<div>
-                    <h1>Mental Health Management System Admin Side</h1>
+                    <h1>Mental Health Management System Admin page Side</h1>
                     <a href="/adminviewquestions">Quiz Management</a><br/>
                     <a href="/DoctorPreview">Doctor Management</a><br/>
                     <a href="/">Post Management</a><br/>
